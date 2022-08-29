e2834c8cef852214b79e6abf467b83fbbb5553a4
Srikanth-LSharma <srikanth.lakshminarayanan@gmail.com>
2022-08-29 23:46:44 +0530

    Font and statateless Widgetaddition

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() =>runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Todo List'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Welcome to Todo List App'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: null,
-      child:Center(
-        child: Text('Add New Item'),
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // removes the debug tag from the app UI
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Todo List',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.white,
+            fontFamily: 'Oswald',
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
       ),
-    ),
-  ),
-));
+      body: Center(
+        child: Text(
+          'Welcome to Todo List App',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.black,
+            fontFamily: 'Oswald',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Add onPressed function here!
+        },
+        label: Text('Add new item'),
+        icon: Icon(Icons.add),
+        backgroundColor: Colors.indigo,
+      ),
+    );
+  }
+}
